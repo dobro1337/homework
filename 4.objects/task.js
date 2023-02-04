dc7218853758648ebd4728aff6521133ecab3f1a
Dobro <dobro_88_88@mail.ru>
2023-02-04 17:51:28 +0300

    Решена задача №1. Инкапсуляция студента

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,27 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  return (this.marks === undefined) ? 0 : (this.marks.reduce((previousValue,currentValue) => previousValue + currentValue)) / this.marks.length; 
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
