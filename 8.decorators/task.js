120bb7cb9f6bc3930e8a296a7cff221a2831bc1c
Dobro <dobro_88_88@mail.ru>
2023-02-18 12:37:06 +0300

    решена задача 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,21 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return function (...args) {
+    let hash = args.join(",");
+    let keys = Object.keys(cache); 
+    if(hash in cache) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    }
+    else{
+      if(keys.length === 5) {
+        delete cache[keys[0]];
+      }
+      cache[hash] = func(...args);
+      console.log("Вычисляем: " + cache[hash]);
+      return "Вычисляем: " + cache[hash];
+    }
+  }
 }
 
 
